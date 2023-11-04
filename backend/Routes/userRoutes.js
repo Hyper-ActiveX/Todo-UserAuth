690dcbbc281a9648eb017f400d65949d004d27f3
Hyper-ActiveX <jayactive11112@gmail.com>
2023-11-04 14:46:51 +0530

    added backend

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { registration, login, dashboard, getUser } = require('../Controllers/userControllers');
+const auth = require("../middleware/auth")
+const cookieParser = require('cookie-parser');
+
+const router = express.Router();
+
+
+
+
+
+
+router.post("/api/u/register", registration);
+router.post("/api/u/login", login);
+router.get("/api/u/user", getUser);
+
+module.exports = router;
---
+++ backend/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+require("./Config/database").ToDoDataBase();
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const todoRouters = require("./Routes/todoRoutes");
+const userRoutes = require("./Routes/userRoutes");
+const cookieParser = require('cookie-parser');
+const auth = require("./middleware/auth");
+const morgan = require('morgan')
+
+
+ 
+app.use(cors({
+  credentials : true,
+  origin : "http://localhost:3000"
+}));
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('combined'))
+
+app.use("/", todoRouters);
+app.use("/", userRoutes);
+// app.use("/", userRoutes);
+
+
+module.exports =app;
